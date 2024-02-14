bde06402c2d018f22ee1e98af9cc4dc47054e53b
Md Habibur Rahman <yourchocomate@gmail.com>
2024-02-14 20:56:32 +0600

    ui v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,22 @@ module.exports = {
   content: ["src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#F9DA2A",
+      },
       fontFamily: {
         unineue: ["Uni Neue", "sans-serif"],
       },
       backgroundImage: () => ({
         "blob-horizontal": "url('../images/bg-horizontal.svg')",
         "blob-vertical": "url('../images/bg-vertical.svg')",
+        "blob": "url('../images/bg-blob.png')",
       }),
       keyframes: {
         "color-bar": {
           "0%": { left: "-100%" },
           "100%": { left: "100%" },
-        }
+        },
       },
       animation: {
         "color-bar": "color-bar 5s linear infinite",
